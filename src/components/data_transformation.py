3858f0b2c697f4b0cfe0e14d92168a67a924f2cd
Shubhra-Awasthi <shubhraawasthi750@gmail.com>
2024-12-21 13:33:45 +0530

    Model Trainer done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,14 +1,17 @@
 import sys
 from dataclasses import dataclass
+import os
+
 import numpy as np
 import pandas as pd
+
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
 from src.exception import CustomException
 from src.logger import logging
-import os
 from src.utils import save_object
 
 @dataclass
--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys #Library which tells where the error occured
+
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
